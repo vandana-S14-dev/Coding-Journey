7e405fca630f5b34d772200801da267ee2eb5fdf
Vandana <its.vandana14@gmail.com>
2026-04-17 22:49:57 +0530

    Day5.py

---
+++ Day5.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in mapping:
+                if not stack or stack[-1] != mapping[char]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(char)
+
+        return len(stack) == 0
+
+obj = Solution()
+
+print(obj.isValid("()"))        # True
+print(obj.isValid("()[]{}"))    # True
+print(obj.isValid("(]"))        # False
+print(obj.isValid("([])"))      # True
+print(obj.isValid("([)]"))      # False
